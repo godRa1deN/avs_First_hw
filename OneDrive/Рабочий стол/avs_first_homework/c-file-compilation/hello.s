08b9b6a83dcbb1f44f578cdaca82b7f4d6c5f92d
Андоец Чугунов <academichug@gmail.com>
2022-09-10 14:25:59 +0300

    commit

---
+++ OneDrive/Рабочий стол/avs_first_homework/c-file-compilation/hello.s
@@ -0,0 +1,21 @@
+	.file	"hello.c"
+	.intel_syntax noprefix
+	.text
+	.section	.rodata
+.LC0:
+	.string	"Hello, world!"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+	push	rbp
+	mov	rbp, rsp
+	lea	rdi, .LC0[rip]
+	mov	eax, 0
+	call	printf@PLT
+	mov	eax, 0
+	pop	rbp
+	ret
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0"
+	.section	.note.GNU-stack,"",@progbits
